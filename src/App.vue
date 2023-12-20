7445465f657603596d9a27cfea219903caedf9e1
Domenico-lel <domenicolella2@gmail.com>
2023-12-20 18:01:53 +0100

    update title example

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,16 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+import AddTitle from './components/AddTitle.vue'
+
+export default {
+  components: {
+    AddTitle
+  }
+}
 </script>
 
 <template>
   <div>
+    <AddTitle />
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
@@ -11,7 +18,6 @@ import HelloWorld from './components/HelloWorld.vue'
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
@@ -21,9 +27,11 @@ import HelloWorld from './components/HelloWorld.vue'
   will-change: filter;
   transition: filter 300ms;
 }
+
 .logo:hover {
   filter: drop-shadow(0 0 2em #646cffaa);
 }
+
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
 }
---
+++ src/components/AddTitle.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+    name: "AddTitle"
+}
+const count = ref(0)
+</script>
+
+<template>
+    <h1 class="read-the-docs">TITOLO</h1>
+</template>
+
+<style scoped>
+.read-the-docs {
+    color: #888;
+}
+</style>
